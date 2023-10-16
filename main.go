c66c4c2c7a5bb6cac81e82324b18c9f13029cdf1
AYUSHMT <48730422+AYUSHMT@users.noreply.github.com>
2023-10-16 10:20:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello world from Ayush Singh Rajput!")
+    fmt.Fprint(w, "Hello world from Ayush Singh Rajput - PR!")
 }
